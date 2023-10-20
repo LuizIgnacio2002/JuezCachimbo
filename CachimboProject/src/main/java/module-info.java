794c26948804a839ddc38cd4af9aba95af2df45a
stevan <83146012+feelware@users.noreply.github.com>
2023-10-20 01:15:58 -0500

    Delete CachimboProject directory

--- CachimboProject/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.cachimboproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.cachimboproject to javafx.fxml;
-    exports com.mycompany.cachimboproject;
-}
